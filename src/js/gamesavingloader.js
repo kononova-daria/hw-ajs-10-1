e63c7fe3da6252df6528159fbfba8e17972f8abc
Kononova Daria <kononova.yu.daria@mail.ru>
2021-09-03 20:19:02 +0300

    Добавлен класс GameSaving

--- src/js/gamesavingloader.js
+++ src/js/gamesavingloader.js
@@ -1,8 +1,9 @@
 import json from './parser';
 import read from './reader';
+import GameSaving from './gamesaving';
 
 export default class GameSavingLoader {
   static load() {
-    return read().then((data) => json(data)).then((saving) => JSON.parse(saving));
+    return read().then((data) => json(data)).then((saving) => new GameSaving(JSON.parse(saving)));
   }
 }
